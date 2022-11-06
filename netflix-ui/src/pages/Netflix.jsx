54df3c8e87caaf590c4e09b9ad51280560d56655
ellisvelandia <eyis619@gmail.com>
2022-11-06 17:07:39 -0500

    navbar

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import backgroundImage from "../assets/home.jpg";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
 
 const Netflix = () => {
-  return (
-    <div>Netflix</div>
-  )
-}
+  const [isScrolled, setIsScrolled] = useState(false);
 
-export default Netflix
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  return (
+    <Container>
+      <Navbar isScrolled={isScrolled} />
+      <div className="hero">
+        <img
+          src={backgroundImage}
+          alt="background"
+          className="background-image"
+        />
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: black;
+  .hero {
+    position: relative;
+    .background-image {
+      filter: brightness(60%);
+    }
+    img {
+      height: 100vh;
+      width: 100vw;
+    }
+    .container {
+      position: absolute;
+      bottom: 5rem;
+      .logo {
+        img {
+          width: 100%;
+          height: 100%;
+          margin-left: 5rem;
+        }
+      }
+      .buttons {
+        margin: 5rem;
+        gap: 2rem;
+        button {
+          font-size: 1.4rem;
+          gap: 1rem;
+          border-radius: 0.2rem;
+          padding: 0.5rem;
+          padding-left: 2rem;
+          padding-right: 2.4rem;
+          border: none;
+          cursor: pointer;
+          transition: 0.2s ease-in-out;
+          &:hover {
+            opacity: 0.8;
+          }
+          &:nth-of-type(2) {
+            background-color: rgba(109, 109, 110, 0.7);
+            color: white;
+            svg {
+              font-size: 1.8rem;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default Netflix;
